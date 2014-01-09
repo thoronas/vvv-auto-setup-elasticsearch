e5ebf6334e4bd1bee97c62ffd7e057fb0286d2e0
thoronas <fsquaredesign@gmail.com>
2014-01-08 20:17:10 -0800

    initial full commit

---
+++ config/site-vars.sh
@@ -0,0 +1,15 @@
+# Modify the variables below to match your project.
+
+# This specifies the main site name in provisioning.
+site_name='Elasticsearch Starter'
+
+# This sets up the name of the DB and the user and password for the DB.
+database='elastic_db'
+dbuser='wp'
+dbpass='wp'
+
+# The site details for this site
+domain='http://elastic-starter.dev'
+admin_user='admin'
+admin_pass='password'
+admin_email='user@example.com'
